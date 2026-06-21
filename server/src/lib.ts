e349a0193c644e0d4f7ed90e9b0c851b881b1473
Roma Shkirman <roma.shkirman@uptail.net>
2026-06-22 02:40:06 +0300

    changed secure to true

--- server/src/lib.ts
+++ server/src/lib.ts
@@ -13,7 +13,7 @@ export const BACKEND_URL = process.env.BACKEND_URL || 'http://localhost:4000';
 export const mailer = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
   port: Number(process.env.SMTP_PORT || 587),
-  secure: false,
+  secure: true,
   auth: process.env.SMTP_USER
     ? {
         user: process.env.SMTP_USER,
